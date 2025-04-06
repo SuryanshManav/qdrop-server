0cc2b5a97005936a38f3b1d0f8cfbf9423f6e752
SuryanshManav <47312357+SuryanshManav@users.noreply.github.com>
2025-04-07 03:01:00 +0800

    Update database.js

--- database.js
+++ database.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-const mongoDB = 'mongodb+srv://manavsuryansh2002:JDRp9iAnZFdAEetn@qdrop-server.8mfvjwi.mongodb.net/';
+const mongoDB = 'mongodb+srv://manavsuryansh2002:JDRp9iAnZFdAEetn@qdrop-server.8mfvjwi.mongodb.net/QDrop';
 
 mongoose.connect(mongoDB, { useNewUrlParser: true, useUnifiedTopology: true, serverSelectionTimeoutMS: 30000 })
   .then(() => console.log('Connected to MongoDB'))
